5ea35b509eae230c3145e709275dfc192e442dd1
Jhon Wick <sv47008546@idat.edu.pe>
2024-04-25 20:43:43 -0500

    avance

--- Semana06Sesion01/d19423/script.js
+++ Semana06Sesion01/d19423/script.js
@@ -1,56 +1,56 @@
-if (typeof(Storage) !== "undefined") {
-    // Code for localStorage/sessionStorage.
-    console.log("Si esta habilitado el webstorae")
-  } else {
-    // Sorry! No Web Storage support..
-    console.log("NO esta habilitado el webstorae")
-  }
+// if (typeof(Storage) !== "undefined") {
+//     // Code for localStorage/sessionStorage.
+//     console.log("Si esta habilitado el webstorae")
+//   } else {
+//     // Sorry! No Web Storage support..
+//     console.log("NO esta habilitado el webstorae")
+//   }
 
-  localStorage.setItem("apellido", "Pineda");
+//   localStorage.setItem("apellido", "Pineda");
 
-  document.getElementById("data").innerHTML = localStorage.getItem("apellido");
+//   document.getElementById("data").innerHTML = localStorage.getItem("apellido");
 
-  let objInfo = {
-    nombre:"Roberto",
-    apellido:"Pineda",
-    edad: 40,
-    hobbies:[
-        "AeroModelismo",
-        "Gunpla"
-    ]
-  }
+//   let objInfo = {
+//     nombre:"Roberto",
+//     apellido:"Pineda",
+//     edad: 40,
+//     hobbies:[
+//         "AeroModelismo",
+//         "Gunpla"
+//     ]
+//   }
 
-  localStorage.setItem("info",JSON.stringify(objInfo));
-console.log(localStorage.getItem("info"));
-  document.getElementById("info").innerHTML = localStorage.getItem("info");
+//   localStorage.setItem("info",JSON.stringify(objInfo));
+// console.log(localStorage.getItem("info"));
+//   document.getElementById("info").innerHTML = localStorage.getItem("info");
 
-  let strHtml = "<ul>";
-  //let objJSON = localStorage.getItem("info");
-  let objJSON = JSON.parse( localStorage.getItem("info"));
+//   let strHtml = "<ul>";
+//   //let objJSON = localStorage.getItem("info");
+//   let objJSON = JSON.parse( localStorage.getItem("info"));
 
-  Object.keys(objJSON).forEach(function(key) {
+//   Object.keys(objJSON).forEach(function(key) {
 
   
-    strHtml+= `<li> ${objJSON[key]} </li>`
+//     strHtml+= `<li> ${objJSON[key]} </li>`
   
-  });
+//   });
   
-  strHtml += "</ul>"
-  document.getElementById("result").innerHTML =strHtml;
+//   strHtml += "</ul>"
+//   document.getElementById("result").innerHTML =strHtml;
 
 
-let sesionObj = sessionStorage.getItem("login");
-if(sesionObj!== null){
-    console.log("Si esta logueado")
-}
-else{
-    console.log("No se ha legueado proceder al login")
-    let objLogin = {
-        user:"rpineda",
-        pass:"1234567890"
-    } 
-    sessionStorage.setItem("login", JSON.stringify(objLogin));
-}
+// let sesionObj = sessionStorage.getItem("login");
+// if(sesionObj!== null){
+//     console.log("Si esta logueado")
+// }
+// else{
+//     console.log("No se ha legueado proceder al login")
+//     let objLogin = {
+//         user:"rpineda",
+//         pass:"1234567890"
+//     } 
+//     sessionStorage.setItem("login", JSON.stringify(objLogin));
+// }
 
 
 
